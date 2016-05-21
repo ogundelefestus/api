f1fdd8d299e33c3553c42f46bb99ce2f42dde592
Serhat Perkmen <serhatperkmen@gmail.com>
2016-05-21 23:34:42 +0300

    update operation implemented

--- app/controllers/BillsController.java
+++ app/controllers/BillsController.java
@@ -3,12 +3,11 @@ package controllers;
 import models.Bill;
 import models.Customer;
 import models.Identity;
-import play.mvc.Controller;
 import play.mvc.Result;
 
 import static services.JsonConcat.concat;
 
-public class BillsController extends Controller {
+public class BillsController extends BaseController {
 
     public Result show(long id) {
 
@@ -17,6 +16,14 @@ public class BillsController extends Controller {
         Identity identity = Identity.find.byId((long) bill.customer_id);
 
         return ok(concat(customer,identity,bill));
+    }
+
+    public Result update (long id) {
+        Bill bill = Bill.find.byId(id);
+        bill.paid = true;
+        bill.payment_date = new java.sql.Date (new java.util.Date().getTime());
+        bill.save();
+        return ok(buildJsonResponse("update", "Users updated successfully"));
     }
 }
 
